52de72338308497609560866d0a916a29f535312
Younghwan <tacdo12@naver.com>
2022-09-30 12:02:53 +0900

    3번 성공 답지봄 ㅋ

--- src/inflearn/ch4/No03SecondTry.java
+++ src/inflearn/ch4/No03SecondTry.java
@@ -16,17 +16,27 @@ public class No03SecondTry {
 		int k = Integer.parseInt(st.nextToken());
 
 		st = new StringTokenizer(br.readLine());
-		ArrayList<Integer> arr = new ArrayList<>();
+		int[] arr = new int[n];
 		int number = 0;
-		while (number != n) {
-			arr.add(Integer.parseInt(st.nextToken()));
-			number++;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < k-1; i++) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+		}
 		
+		for (int i = k-1; i < arr.length; i++) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+			sb.append(map.size()+" ");
+			map.put(arr[i-k+1], map.get(arr[i-k+1])-1);
+			if(map.get(arr[i-k+1])==0) {
+				map.remove(arr[i-k+1]);
+			}
+		}
 		
-		
+		System.out.println(sb.toString());
 	}
 
 }
