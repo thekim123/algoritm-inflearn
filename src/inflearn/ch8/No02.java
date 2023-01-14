aadee179bb0950933a923dbdccb9bad8d2bbe69e
thekim123 <tacdo12@naver.com>
2023-01-14 22:35:11 +0900

    최대점수 구하기 DFS 완료

--- src/inflearn/ch8/No02.java
+++ src/inflearn/ch8/No02.java
@@ -1,4 +1,40 @@
 package ch8;
 
+import java.util.Scanner;
+
 public class No02 {
+
+    static int level = 0;
+    static int total;
+    static int answer = 0;
+
+    static void DFS(int lev, int sum, int[] arr) {
+        if (sum > total) {
+            return;
+        }
+
+        if (answer < sum) {
+            answer = sum;
+        }
+
+        if (lev < arr.length) {
+            DFS(lev + 1, sum, arr);
+            DFS(lev + 1, sum + arr[lev], arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        total = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+
+
 }
